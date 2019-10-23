d7480c680609ceb3db9140b1d9f0216b7dc2c255
adrian <adriangohjw@gmail.com>
2019-10-24 02:57:54 +0800

    added java project

---
+++ MOBLIMA/src/app/App.java
@@ -0,0 +1,8 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // default generated (placeholder for the time being)
+        System.out.println("Hello Java");
+    }
+}
