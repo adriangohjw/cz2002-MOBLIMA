a7d22e2503482eaad94cdc195ba2b285e8f1f992
adrian <adriangohjw@gmail.com>
2019-10-24 04:23:11 +0800

    added Class Admin and Movie_Goer

---
+++ MOBLIMA/src/app/Admin.java
@@ -0,0 +1,17 @@
+package app;
+
+import java.security.NoSuchAlgorithmException;
+
+/*
+ * CRUD movie listing
+ * CRUD cinema, showtimes and movies (details)
+ * CRUD top viewing movies by ticket sales OR ratings
+ */
+
+public class Admin extends User {
+	
+	public Admin(String username, String password, int role) throws NoSuchAlgorithmException {
+		super(username, password, ADMIN);
+	}
+	
+}
---
+++ MOBLIMA/src/app/Movie_Goer.java
@@ -0,0 +1,42 @@
+package app;
+
+import java.security.NoSuchAlgorithmException;
+
+/*
+ * Movie_Goer is able to do the following:
+ * - See seat availability
+ * - See ratings
+ * - see movie details
+ * - See list of movies 
+ * - Search for movies
+ * - CR transactions
+ * - R top movies (by ticket sales/listing)
+ * - C Rating
+ */
+
+public class Movie_Goer extends User {
+	
+	private String name;
+	private String mobileNumber;
+
+	// user can create account without name and mobile number 
+	public Movie_Goer(String username, String password, int role) throws NoSuchAlgorithmException {
+		super(username, password, MOVIE_GOER);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+}
