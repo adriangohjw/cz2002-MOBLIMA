a00938bae03d635546f3cec4386e7ff852a20db3
adrian <adriangohjw@gmail.com>
2019-11-09 23:06:26 +0800

    update Admin class to fix value of role to 1

--- MOBLIMA/src/Model/Admin.java
+++ MOBLIMA/src/Model/Admin.java
@@ -10,7 +10,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class Admin extends User {
 	
-	public Admin(String username, String password, int role) throws NoSuchAlgorithmException {
+	public Admin(String username, String password) throws NoSuchAlgorithmException {
 		super(username, password, ADMIN);
 	}
 	
