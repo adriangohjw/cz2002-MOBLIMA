b5c7a370a628b51aed1eda89229971d53f8d1cea
koapham <47772920+koapham@users.noreply.github.com>
2019-11-10 18:23:14 +0800

    Update SearchMovieUI.java

--- MOBLIMA/src/Boundary/SearchMovieUI.java
+++ MOBLIMA/src/Boundary/SearchMovieUI.java
@@ -44,7 +44,7 @@ public class SearchMovieUI {
         System.out.println("Enter movie title: ");
         title = sc.next();
         ArrayList<Movie> movieList = moviesController.readByAttribute(0, title); //there are two readByAttribute, which to be used?
-        movieList.forEach(movie -> printMovie(movie));
+        movieList.forEach(movie -> movie.toString);
     }
 
 
