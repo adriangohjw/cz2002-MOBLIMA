d4c0fa1f525082e6a65268f997582d5281337e2c
reenaxxraj <44928185+reenaxxraj@users.noreply.github.com>
2019-11-11 03:04:12 +0800

    Update MainMenuUI.java

--- MOBLIMA/src/Boundary/MainMenuUI.java
+++ MOBLIMA/src/Boundary/MainMenuUI.java
@@ -1,43 +1,115 @@
 package Boundary;
 
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.Scanner; 
 
-public class CURshowtimes {
-	Scanner input = new Scanner(System.in);
+public class MainMenuUI {
 	
-	public void main() throws ClassNotFoundException, IOException{
-		System.out.println("Create/Update/Remove session: /n" +
-						   "1. Create Movie Session/n" +
-						   "2. Update Movie Session/n" +
-						   "3. Remove Movie Session");
-		int option = input.nextInt();
-		switch(option) {
-		case 1: 
-			createSession();
-			break;
-		case 2:
-			updateMovie();
-			break;
-		case 3:
-			removeMovie();
-			break;
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to MOBLIMA!\n" +
+						   "1. Admin\n" +
+						   "2. Movie Goer\n" +
+						   "3. Exit");
+		int choice = input.nextInt();
+		boolean exit = false;
+		while (!exit) {
+		switch(choice) {
+			case 1:
+				admin_login();
+				break;
+			case 2:
+				movie_goer();
+				break;
+			case 3:
+				exit = true;
+				System.out.println("Exiting MOBLIMA");
+				break;
+			default:
+				System.out.println("Wrong input!");
+			}
+			
+		}
+	}
+	
+	public static void admin_login() throws ClassNotFoundException, IOException {
+		LoginUI admin_login = new LoginUI();
+		admin_login.main();
+		boolean log_out = false;
+		Scanner input = new Scanner(System.in);
+		while (log_out) {
+			System.out.println("Select action:\n"+
+					   "1.Create/Update/Remove movie listing\n"+
+					   "2.Create/Update/Remove movie session\n"+
+					   "3.Configure system settings\n" +
+					   "4.Log out");
+			int choice = input.nextInt();
+			switch(choice) {
+				case 1:
+					CUR_Movie_Listing.main();
+					break;
+				case 2:
+					CURshowtimes CUR2 = new CURshowtimes();
+					CUR2.main();
+					break;
+				case 3:
+					ConfigureSystemSetting Conf = new ConfigureSystemSetting();
+					Conf.main();
+					break;
+				case 4:
+					log_out = true;
+					System.out.println("Logged out successfully!");
+					break;
+				default:
+					System.out.println("Try again!");
+					break;
+			}
+					
+		}
+	}
+	
+	public static void movie_goer() {
+		Scanner input = new Scanner(System.in);
+		boolean exit = false;
+		while (!exit) {
+			System.out.println("Select action:\n"+
+					   "1.Search/List movie\n"+
+					   "2.View movie details\n"+
+					   "3.Check seat availibility\n" +
+					   "4.Book ticket\n" +
+					   "5.View booking history\n" +
+					   "6.List Top 5 movies\n" +
+					   "7.exit");
+			int choice = input.nextInt();
+			switch(choice) {
+				case 1:
+					
+					break;
+				case 2:
+
+					break;
+				case 3:
+
+					break;
+				case 4:
+
+					break;
+				case 5:
+					break;
+				case 6:
+					break;
+				case 7:
+					break;
+				default:
+					System.out.println("Try again!");
+					break;
+			}
+					
 		}
 
-	public void createSession() {
-		System.out.println("Creating Session...");
-		System.out.println("Enter session code: ");
-		String cinemaCode = input.nextLine();
-		System.out.println("Enter movie title: ");
-	    String movieTitle = input.nextLine();
-	    
-	    System.out.println("Enter session date: ");
-	    Date session
-	    Date sessionDate,
-	    String sessionTime,
-	    SeatsAvailability seatsAvailability
 	}
 }
+
 
 // package Boundary;
 
