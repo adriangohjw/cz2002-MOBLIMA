1776e3402ef36a6966591d3fd88099f22a5a706e
koapham <47772920+koapham@users.noreply.github.com>
2019-11-10 18:25:15 +0800

    Update ViewMovieDetailUI.java

--- MOBLIMA/src/Boundary/ViewMovieDetailUI.java
+++ MOBLIMA/src/Boundary/ViewMovieDetailUI.java
@@ -25,42 +25,13 @@ public class ViewMovieDetailUI {
         System.out.println("Enter the movie title");
         title = sc.next();
         ArrayList<Movie> movieList = moviesController.readByAttribute(0, title);
-        movieList.forEach(movie -> printMovie(movie));
+        movieList.forEach(movie -> movie.toString());
         
         System.out.println("Another movie? 1 for [y] and 0 for [no]");
         another = sc.nextInt();
 
         if(another==0){
             System.out.println("Exit!");
-        }
-    }
-
-    public void printMovie(Movie movie){
-        System.out.println("Title: "+ movie.getTitle());
-        System.out.println("Type: " + movie.getType());
-        System.out.println("Synopsis: " + movie.getSynopsis());
-        System.out.println("Status: " + movie.getShowStatus());
-        System.out.print("Director: " + movie.getDirector() + "  |  Cast: ");
-        movie.getCast().forEach(cast -> System.out.print(cast + "   "));
-        System.out.println("Rating: " + movie.getRating());
-        System.out.println("Review: ");
-        ArrayList<Review> reviews = movie.getReviews();
-        int size = reviews.size();
-        if(size <=3 ){
-            reviews.forEach(review->printReview(review));
-        } else {
-            for (int i=0; i<3; i++){
-                printReview(reviews.get(i));
-            }
-        }
-        
-        System.out.println("---------------------");
-    }
-    
-    public void printReview(Review review){
-        System.out.print("Star: " + review.getNumOfStars());
-        if(review.getAdditionalComment() != null){
-            System.out.println(" | " + review.getAdditionalComment());
         }
     }
     
