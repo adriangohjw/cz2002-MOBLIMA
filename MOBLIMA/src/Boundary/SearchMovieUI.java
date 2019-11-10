2212c2d23b97ff37e69d3d30f12b019988ceb982
koapham <47772920+koapham@users.noreply.github.com>
2019-11-11 00:34:37 +0800

    Update SearchMovieUI.java

--- MOBLIMA/src/Boundary/SearchMovieUI.java
+++ MOBLIMA/src/Boundary/SearchMovieUI.java
@@ -12,13 +12,13 @@ public class SearchMovieUI {
     private String cast;
     private int option;
     private MoviesController moviesController = new MoviesController();
-
+    
     Scanner sc = new Scanner(System.in);
 
     SearchMovieUI (){};
 
     public void main(){
-        while(option!=3){
+        while(option!=4){
             display();
         }
     }
@@ -28,14 +28,17 @@ public class SearchMovieUI {
         System.out.println("Hello, you can search for movie by: ");
         System.out.println("1. Search by movie title");
         System.out.println("2. Search by movie type");
-        System.out.println("3. Exit");
+        System.out.println("3. List all movie title");        
+        System.out.println("4. Exit");
         option = sc.nextInt();
             switch(option){
             case 1: searchByTitle();
                     break;
             case 2: searchByType();
                     break;
-            case 3: System.out.println("Exit!");
+            case 3: listAllMovies();
+                    break;         
+            case 4: System.out.println("Exit!");
         }
     }
 
@@ -44,7 +47,8 @@ public class SearchMovieUI {
         System.out.println("Enter movie title: ");
         title = sc.next();
         ArrayList<Movie> movieList = moviesController.readByAttribute(0, title); //there are two readByAttribute, which to be used?
-        movieList.forEach(movie -> movie.toString);
+        movieList.forEach(movie -> printMovie(movie));
+        viewMovieDetail();
     }
 
 
@@ -52,7 +56,26 @@ public class SearchMovieUI {
         System.out.println("Enter movie type: ");
         type = sc.next();
         ArrayList<Movie> movieList = moviesController.readByAttribute(1, type);
-        movieList.forEach(movie -> movie.toString());
+        movieList.forEach(movie -> printMovie(movie));
+        viewMovieDetail();
     }
-
+   
+    
+    public void viewMovieDetail(){
+        System.out.println("Choose movie to view detail: ");
+        title = sc.next();
+        ViewMovieDetailUI viewMovieDetailUI = new ViewMovieDetailUI(title);
+        viewMovieDetailUI.display();            
+    }
+    
+    public void listAllMovies(){
+        ArrayList<Movie> movieList = moviesController.read();
+        movieList.forEach(movie -> printMovie(movie));
+    }
+    
+    public void printMovie(Movie movie){
+        System.out.println("Title: " + movie.getTitle());
+        System.out.println("Release Date: " + movie.getMovieReleaseDate());
+        System.out.println("-------------------");
+    }
 }
