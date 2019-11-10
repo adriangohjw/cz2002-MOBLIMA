682dd760ca81abd5b55808db71bd7f95030fa140
koapham <47772920+koapham@users.noreply.github.com>
2019-11-10 10:52:11 +0800

    Update SearchMovieUI.java

--- MOBLIMA/src/Boundary/SearchMovieUI.java
+++ MOBLIMA/src/Boundary/SearchMovieUI.java
@@ -19,7 +19,7 @@ public class SearchMovieUI {
     SearchMovieUI (){};
 
     public void main(){
-        while(option!=5){
+        while(option!=3){
             display();
         }
     }
@@ -29,20 +29,14 @@ public class SearchMovieUI {
         System.out.println("Hello, you can search for movie by: ");
         System.out.println("1. Search by movie title");
         System.out.println("2. Search by movie type");
-        System.out.println("3. Search by director");
-        System.out.println("4. Search by cast");
-        System.out.println("5. Exit");
+        System.out.println("3. Exit");
         option = sc.nextInt();
             switch(option){
             case 1: searchByTitle();
                     break;
             case 2: searchByType();
                     break;
-            case 3: searchByDirector();
-                    break;
-            case 4: searchByCast();
-                    break;
-            case 5: System.out.println("Exit!");
+            case 3: System.out.println("Exit!");
         }
     }
 
@@ -50,7 +44,7 @@ public class SearchMovieUI {
     public void searchByTitle(){
         System.out.println("Enter movie title: ");
         title = sc.next();
-        Movie[] movieList = movieRatingsController.readByTitle(title);
+        ArrayList<Movie> movieList = MoviesController.readByAttribute(0, title);
         movieList.forEach(movie -> printMovie(movie));
     }
 
@@ -58,33 +52,17 @@ public class SearchMovieUI {
     public void searchByType(){
         System.out.println("Enter movie type: ");
         type = sc.next();
-        Movie[] movieList = movieRatingsController.readByType(type);
+        ArrayList<Movie> movieList = MoviesController.readByAttribute(1, type);
         movieList.forEach(movie -> printMovie(movie));
     }
-    //How can search for Sysnopsis because the description cannot be exactly the same
-
-
-    public void searchByDirector(){
-        System.out.println("Enter director: ");
-        director = sc.next();
-        Movie[] movieList = movieRatingsController.readByDirector(director);
-        movieList.forEach(movie -> printMovie(movie));
-    }
-
-
-    public void searchByCast(){
-        System.out.println("Enter cast: ");
-        cast = sc.next();
-        Movie[] movieList = movieRatingsController.readByCast(cast);
-        movieList.forEach(movie -> printMovie(movie));
-    }
-
 
     public void printMovie(Movie movie){
-        System.out.println(movie.getTitle());
-        System.out.println(movie.getType());
-        System.out.println(movie.getSynopsis());
-        System.out.println(movie.getShowStatus());
+        System.out.println("Title: "+ movie.getTitle());
+        System.out.println("Type: " + movie.getType());
+        System.out.println("Synopsis: " + movie.getSynopsis());
+        System.out.println("Status: " + movie.getShowStatus());
+        System.out.print("Director: " + movie.getDirector() + "  |  Cast: ");
+        movie.getCast().forEach(cast -> System.out.print(cast + "   "));
         System.out.println("---------------------");
     }   
 
