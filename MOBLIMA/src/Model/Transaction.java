b2131ac541e29fd378ecb91674bca5635f2d8c13
Kamil Rogoda <rogoda.kamil@gmail.com>
2019-11-14 00:42:33 +0800

    Add Movie object into Transaction class

--- MOBLIMA/src/Model/Transaction.java
+++ MOBLIMA/src/Model/Transaction.java
@@ -6,13 +6,15 @@ import java.text.SimpleDateFormat;
 
 public class Transaction implements Serializable {
 
-	private String TID; 		// XXXYYYYMMDDhhmm (XXX is cinema code and YYYYDDMMhhmm is datetime)
+	private String TID; // XXXYYYYMMDDhhmm (XXX is cinema code and YYYYDDMMhhmm is datetime)
 	private Movie_Goer movieGoer;
+	private Movie movie;
 	
-	public Transaction(String cinemaCode, Movie_Goer movieGoer) {
+	public Transaction(String cinemaCode, Movie_Goer movieGoer, Movie movie) {
 		String formattedTimestamp = new SimpleDateFormat("YYYYMMddHHmm").format(new Date());
 		this.TID = cinemaCode.concat(formattedTimestamp);
 		this.movieGoer = movieGoer;
+		this.movie = movie;
 	}
 
 	public String getTID() {
@@ -23,12 +25,17 @@ public class Transaction implements Serializable {
 		return movieGoer;
 	}
 
+	public Movie getMovie(){
+		return movie;
+	}
+
 	public String toString(){
 		String toReturn = "";
 		toReturn 	+= "TID: " + getTID() + "\n"
 					+ "Name: " + getMovieGoer().getName() + "\n"
 					+ "Mobile number: " + getMovieGoer().getMobileNumber() + "\n"
-					+ "Email: " + getMovieGoer().getEmail();
+					+ "Email: " + getMovieGoer().getEmail()+ "\n"
+					+ "Movie: " + getMovie().getTitle();
 		return toReturn; 
 	}
 }
